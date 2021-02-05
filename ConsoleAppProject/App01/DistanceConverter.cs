24dc0a334d233358020fc18acf29fa1a8457d906
liam smith <lsmith2201@outlook.com>
2021-02-05 01:19:41 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,61 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// An App to convert miles to feet
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Liam Smith version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// /// <author>
+        /// Liam Smith version 0.1
+        /// </author>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Promt the user to enter the distance in miles
+        /// Input the miles in a double number
+        /// </summary>
+        /// /// <author>
+        /// Liam Smith version 0.1
+        /// </author>
+        private void InputMiles()
+        {
+
+        }
+
+        /// <summary>
+        /// Convert the input to feet
+        /// Multiply miles * 5280
+        /// </summary>
+        /// /// <author>
+        /// Liam Smith version 0.1
+        /// </author>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// Display result of the previous calculation to the user
+        /// </summary>
+        /// /// <author>
+        /// Liam Smith version 0.1
+        /// </author>
+        private void OutputFeet()
+        {
+
+        }
     }
 }
